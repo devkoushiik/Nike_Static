d6aca0848416ddb3963c87928a3ff8080a02bce8
koushik ahmed <koushikxahmed@gmail.com>
2025-02-03 14:35:28 +0600

    final website done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import Hero from "./sections/Hero";
+import Nav from "./components/Nav";
+import {
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
 
 const App = () => {
-  return <div className="uppercase bg-slate-600 font-bold">App</div>;
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className=" bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
 };
 
 export default App;
